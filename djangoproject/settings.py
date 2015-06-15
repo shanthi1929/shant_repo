6750d5c5a54d7a435ea2a274afd1b7dc53edb098
shanthz <shanthi.kalidass@gmail.com>
2015-06-15 14:21:31 +0530

    database and tables created using mysql

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -59,8 +59,13 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first_django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
